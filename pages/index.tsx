9104b7dbe0fc1bdbba2a64be091d212171e4184d
Alex Barraza <diegobarrazapascual@gmail.com>
2021-05-27 13:46:50 -0500

    added fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://use.typekit.net/luh8isz.css" />
       </Head>
 
       <main className={styles.main}>
