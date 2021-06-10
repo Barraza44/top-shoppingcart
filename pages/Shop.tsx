ab705daa520cbedd89a33625cbda999aa0015ef1
Alex Barraza <diegobarrazapascual@gmail.com>
2021-06-09 21:15:34 -0500

    imported plant component

--- pages/Shop.tsx
+++ pages/Shop.tsx
@@ -3,36 +3,46 @@ import Head from "next/head";
 import Header from "../components/Header";
 import styles from "../styles/Shop.module.css"
 import plantData from "../plantData.json";
+import PlantCard from "../components/PlantCard";
 
-export default function Shop() {
-  return(
+export default function Shop({PlantData}) {
+  return (
     <>
-    <Head>
-      <title>The Dock | Shop </title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
-      <link rel="stylesheet" href="https://use.typekit.net/luh8isz.css" />
-    </Head>
+      <Head>
+        <title>The Dock | Shop </title>
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,400&display=swap"
+          rel="stylesheet"/>
+        <link rel="stylesheet" href="https://use.typekit.net/luh8isz.css"/>
+      </Head>
 
-    <Header headerStyle={"header2"} BoxColor={"Black"} />
-    <main className={styles.page}>
-      <h2 className={styles.title}>Shop</h2>
-      <div className={styles.content}>
-        <aside>
-          <p>A list</p>
-        </aside>
-        <div className={styles.products}>
-          <p>Product</p>
+      <Header headerStyle={"header2"} BoxColor={"Black"}/>
+      <main className={styles.page}>
+        <h2 className={styles.title}>Shop</h2>
+        <div className={styles.content}>
+          <aside>
+            <p>A list</p>
+          </aside>
+          <div className={styles.products}>
+            {PlantData.map(plant => (
+              <PlantCard
+                key={plant.id}
+                src={plant.src}
+                name={plant.name}
+                price={plant.price}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
     </>
   )
 }
 
 export function getStaticProps() {
   return {
-    props: plantData
+    props: { plantData }
   }
 }
