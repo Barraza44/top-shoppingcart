cad600ca500d03a8121ac693ba950a6af51d7243
Alex Barraza <diegobarrazapascual@gmail.com>
2021-06-09 23:13:37 -0500

    added content for plant card, added plant data

--- components/PlantCard.tsx
+++ components/PlantCard.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Image from "next/image";
+import styles from "../styles/Card.module.css"
 
 export default function PlantCard({src, name, price}) {
-  return(
-    <div>
-      <Image
-        src={src}
-        width={295}
-        height={295}
-        objectFit={"cover"}
-      />
-      <p>{name}</p>
-      <p>{price}</p>
+  return (
+    <div className={styles.card}>
+      <div className={styles.images}>
+        <Image
+          src={src}
+          width={298}
+          height={295}
+          objectFit={"cover"}
+        />
+      </div>
+      <p className={styles.name}>{name}</p>
+      <p className={styles.price}>{`$${price}`}</p>
     </div>
   )
 }
