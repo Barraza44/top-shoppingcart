f846ad831baabb73d323e6277faa91b0b399a625
Alex Barraza <diegobarrazapascual@gmail.com>
2021-06-12 23:19:53 -0500

    added CategoryList component styles

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "../styles/Category.module.css";
 
 export default function CategoryList() {
   return (
     <aside>
-      <h2>Plants</h2>
+      <h2 className={styles.title}>Plants</h2>
       <ul>
         <li>Large</li>
         <li>Medium</li>
