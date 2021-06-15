e05b44c2ed52d8cd99a1e310e8e262dfe24765b5
Alex Barraza <diegobarrazapascual@gmail.com>
2021-06-14 22:32:07 -0500

    added animation for list items, dynamic route for product id

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "../styles/Category.module.css";
+import Link from "next/link"
 
 export default function CategoryList() {
   return (
     <aside>
       <h2 className={styles.title}>Plants</h2>
       <ul className={styles.list}>
-        <li className={styles.listItem}>Large</li>
-        <li className={styles.listItem}>Medium</li>
-        <li className={styles.listItem}>Small</li>
+        <li className={styles.listItem}><Link href="/Shop/Large">Large</Link></li>
+        <li className={styles.listItem}><Link href="/Shop/Medium">Medium</Link></li>
+        <li className={styles.listItem}><Link href="/Shop/Small">Small</Link></li>
       </ul>
     </aside>
   )
